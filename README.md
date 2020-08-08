b877e7facb35da266f5f482f5e0b4da39207ed1d
kapil <kapil.kathuria@gmail.com>
2020-08-08 18:22:48 +0530

    section 1 - swagger intro completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lrn_swagger
+
+Course : swagger with nodejs - https://www.udemy.com/course/nodejs-api-development-with-swagger/learn/lecture/18187422#overview. 
+
+Sample repos of iinstructor: https://github.com/jgomes94?tab=repositories
---
+++ section 1.js
@@ -0,0 +1,35 @@
+// https://www.udemy.com/course/nodejs-api-development-with-swagger/learn/lecture/18187422#overview
+
+// Covers
+// using open api
+// use oas 
+// combine oas, swagger and nodejs
+// swagger architecture
+// create rest api with swagger
+// deploy and mange api with security (jwt and token)
+
+// overview
+// api example: translation, payment gateway, logon
+// swagger: build and prototype
+//  offers Open API specification
+
+// OAS Processor and editor
+// Benefits
+//  Security
+//  reduces code needed
+//  ui based interaction and documentation generation
+
+// swagger documentation
+// shows rules onclient and server side
+// 
+
+// swagger
+// 1. validates the request
+// 2. sends to nodejs if request is valid
+
+// swgger with nodejs and express
+// express works on top of http modules of nodeja
+// swagger works on top of express to make things easier
+
+
+
