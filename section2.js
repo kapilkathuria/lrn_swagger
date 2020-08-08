97461e71a24fb6f5c23b9ce7cd24530ffce810e9
kapil <kapil.kathuria@gmail.com>
2020-08-08 18:33:57 +0530

    section 2 - video 1 done

---
+++ section2.js
@@ -0,0 +1,13 @@
+// 
+
+// http and swagger
+// http request and response
+// returned is http header and http body
+
+// validations
+// validation on http body
+// validation on http end point parameters
+// as suach no validations is required in code
+// validations types
+//  string, booleans, arrays [], integerrs, objects {'key':''value'}
+
